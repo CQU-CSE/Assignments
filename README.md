65381fa161da1a153950c3c0e8f822e074f179fa
Coder-Yu <yuyu2223@163.com>
2017-09-30 22:26:56 +0800

    add new assignment 101

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Assignments
 Assignments for newbies (students of Prof. Gao)
+
+<h2>Assignment 101 </h2>
+<div>
+<p>
+<b>练习内容：</b>熟练 KNN（K 邻近）算法的思想和编码实现
+</p>
+
+<p>
+<b>详细说明：</b>利用 python 语言实现 KNN 算法，并对收集的鸢尾花数据进行分类。鸢尾花数据
+分为两部分，一部分为 104 个带类别标记的样本组成的训练集，另一部分为不带类别标记的
+46 个样本组成的测试集，要求对 46 个不带类别标记的样本进行预测。压缩包内含4个文件，
+分别为 data-description.txt, training.txt, test.txt,knn.py。knn.py 为需要你来完成的代码。
+</p>
+<p>
+<b>输入与输出：</b>输入训练集与测试集数据（见文本文件），输出分类正确率，召回率，F1
+</p>
+<P>
+<b>注意：</b>请尽量不要调用第三方的直接实现，如scikit-learn
+</P>
+</div>
---
+++ assignment 101/knn.py
@@ -0,0 +1,34 @@
+#coding:utf8
+import sys
+import numpy as np
+
+answer = []
+trainSet = []
+trainLabel = []         #labels of the samples in the training set
+K = 5                   #you can choose another K value
+     
+
+def classify():
+    #training
+    with open('./training.txt') as f:
+        for line in f:
+           #you can add your core code here
+
+    #test
+    with open('./test.txt') as f:
+        for line in f:
+           #add your code here
+
+
+	
+	
+	
+	
+	
+	
+if __name__ == '__main__':
+
+    classify()
+
+    
+
